be372d49afdf96b00d4bd13c61424be4e05c1555
Jun-ya HASEBA <7pairs@gmail.com>
2019-03-05 19:45:35 +0900

    Update with temporary implementation #5

--- src/main/java/blue/lions/revdoc/RevDoc.java
+++ src/main/java/blue/lions/revdoc/RevDoc.java
@@ -1,7 +1,17 @@
 package blue.lions.revdoc;
 
+import blue.lions.revdoc.ast.Node;
+import blue.lions.revdoc.parser.ReviewParser;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
+import org.parboiled.Parboiled;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParsingResult;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class RevDoc {
 
@@ -10,7 +20,20 @@ public class RevDoc {
         CmdLineParser cmdLineParser = new CmdLineParser(arguments);
         try {
             cmdLineParser.parseArgument(args);
+
+            String input = "= 見出し\n\n1行目\n2行目\n\n2段落目\n";
+            ReviewParser reviewParser = Parboiled.createParser(ReviewParser.class);
+            ParsingResult<Node> result = new ReportingParseRunner<Node>(reviewParser.Root()).run(input);
+
+            XWPFDocument document = new XWPFDocument();
+            result.resultValue.export(document);
+            FileOutputStream fout = new FileOutputStream("~/test.docx");
+            document.write(fout);
         } catch (CmdLineException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/blue/lions/revdoc/parser/ReviewParser.java
+++ src/main/java/blue/lions/revdoc/parser/ReviewParser.java
@@ -1,4 +1,73 @@
 package blue.lions.revdoc.parser;
 
-public class ReviewParser {
+import blue.lions.revdoc.ast.Heading1Node;
+import blue.lions.revdoc.ast.Node;
+import blue.lions.revdoc.ast.ParagraphNode;
+import blue.lions.revdoc.ast.RootNode;
+import org.parboiled.BaseParser;
+import org.parboiled.Rule;
+import org.parboiled.annotations.BuildParseTree;
+
+@BuildParseTree
+public class ReviewParser extends BaseParser<Object> {
+
+    public Rule Root() {
+        return Sequence(
+            push(new RootNode()),
+            ZeroOrMore(Block(), appendChild())
+        );
+    }
+
+    public Rule Block() {
+        return Sequence(
+            ZeroOrMore(BlankLine()),
+            FirstOf(Heading1(), Paragraph())
+        );
+    }
+
+    public Rule Heading1() {
+        return Sequence(
+            "= ",
+            OneOrMore(NormalCharacter()),
+            push(new Heading1Node(match())),
+            Newline()
+        );
+    }
+
+    public Rule Paragraph() {
+        return Sequence(
+            OneOrMore(
+                OneOrMore(NormalCharacter()),
+                Newline()
+            ),
+            push(new ParagraphNode(match())),
+            Test(FirstOf(OneOrMore(BlankLine()), EOI))
+        );
+    }
+
+    public Rule NormalCharacter() {
+        return Sequence(
+            TestNot(' '),
+            TestNot(AnyOf("\n\r")),
+            ANY
+        );
+    }
+
+    public Rule Newline() {
+        return FirstOf('\n', Sequence('\r', Optional('\n')));
+    }
+
+    public Rule BlankLine() {
+        return Sequence(
+            ZeroOrMore(' '),
+            Newline()
+        );
+    }
+
+    public boolean appendChild() {
+        Node child = (Node) pop();
+        Node parent = (Node) peek();
+        parent.appendChild(child);
+        return true;
+    }
 }

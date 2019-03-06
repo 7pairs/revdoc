e3a0730db8668e83293aa4ac7ad61d0c076ea80b
Jun-ya HASEBA <7pairs@gmail.com>
2019-03-06 09:13:30 +0900

    Update with temporary implementation #5

--- src/main/java/blue/lions/revdoc/RevDoc.java
+++ src/main/java/blue/lions/revdoc/RevDoc.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Jun-ya HASEBA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package blue.lions.revdoc;
 
 import blue.lions.revdoc.ast.Node;
@@ -13,14 +28,23 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * revdocのメインクラス。
+ */
 public class RevDoc {
 
+    /**
+     * メイン処理。
+     *
+     * @param args コマンドライン引数
+     */
     public static void main(String... args) {
-        Arguments arguments = new Arguments();
-        CmdLineParser cmdLineParser = new CmdLineParser(arguments);
-        try {
-            cmdLineParser.parseArgument(args);
+        // コマンドライン引数をパースする
+        Arguments arguments = parseArgument(args);
 
+        Config config = parseConfig(arguments.getInputDirectory());
+
+        try {
             String input = "= 見出し\n\n1行目\n2行目\n\n2段落目\n";
             ReviewParser reviewParser = Parboiled.createParser(ReviewParser.class);
             ParsingResult<Node> result = new ReportingParseRunner<Node>(reviewParser.Root()).run(input);
@@ -29,12 +53,31 @@ public class RevDoc {
             result.resultValue.export(document);
             FileOutputStream fout = new FileOutputStream("~/test.docx");
             document.write(fout);
-        } catch (CmdLineException e) {
-            e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /*
+     * コマンドライン引数をパースして @{code Arguments} に格納する。
+     *
+     * @param args コマンドライン引数
+     * @return コマンドライン引数オブジェクト
+     */
+    private static Arguments parseArgument(String... args) {
+        Arguments arguments = new Arguments();
+        CmdLineParser cmdLineParser = new CmdLineParser(arguments);
+        try {
+            cmdLineParser.parseArgument(args);
+        } catch (CmdLineException e) {
+            e.printStackTrace();
+        }
+        return arguments;
+    }
+
+    private static Config parseConfig(String configPath) {
+        return new Config();
     }
 }

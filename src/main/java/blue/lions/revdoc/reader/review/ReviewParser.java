f5acdf8d50a8f5e67162073e62d44f94b7baef17
Jun-ya HASEBA <7pairs@gmail.com>
2019-04-25 18:58:40 +0900

    Update ReviewParser class #30

--- src/main/java/blue/lions/revdoc/reader/review/ReviewParser.java
+++ src/main/java/blue/lions/revdoc/reader/review/ReviewParser.java
@@ -130,7 +130,7 @@ class ReviewParser extends BaseParser<Object> {
 
     /*
      * ContentText = !Newline !Headline !SingleLineComment !BlockElement !UList !OList !DList
-     *               ( !InlineElement [^\r\n] )+
+     *               (!InlineElement [^\r\n])+
      */
     Rule ContentText() {
         return Sequence(
@@ -204,6 +204,421 @@ class ReviewParser extends BaseParser<Object> {
             ColumnContent(),
             Optional(ColumnContents()),
             BlankLines()
+        );
+    }
+
+    /*
+     * ColumnContent = !ColumnTerminator SingleLineComment
+     *               / !ColumnTerminator BlockElement
+     *               / !ColumnTerminator UList
+     *               / !ColumnTerminator OList
+     *               / !ColumnTerminator DList
+     *               / !ColumnTerminator Paragraph
+     *               / !ColumnTerminator Chapter
+     */
+    Rule ColumnContent() {
+        return FirstOf(
+            Sequence(TestNot(ColumnTerminator()), SingleLineComment()),
+            Sequence(TestNot(ColumnTerminator()), BlockElement()),
+            Sequence(TestNot(ColumnTerminator()), UList()),
+            Sequence(TestNot(ColumnTerminator()), OList()),
+            Sequence(TestNot(ColumnTerminator()), DList()),
+            Sequence(TestNot(ColumnTerminator()), Paragraph()),
+            Sequence(TestNot(ColumnTerminator()), Chapter())
+        );
+    }
+
+    /*
+     * ColumnTerminator = "="+ "[/column]" Space* BlankLines
+     */
+    Rule ColumnTerminator() {
+        return Sequence(
+            OneOrMore("="),
+            "[/column]",
+            ZeroOrMore(Space()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * BracketArg = "[" BracketArgSubs "]"
+     */
+    Rule BracketArg() {
+        return Sequence(
+            "[",
+            BracketArgSubs(),
+            "]"
+        );
+    }
+
+    /*
+     * BracketArgSubs = BracketArgSub BracketArgSubs?
+     */
+    Rule BracketArgSubs() {
+        return Sequence(
+            BracketArgSub(),
+            Optional(BracketArgSubs())
+        );
+    }
+
+    /*
+     * BracketArgSub = InlineElement / BracketArgText
+     */
+    Rule BracketArgSub() {
+        return FirstOf(
+            InlineElement(),
+            BracketArgText()
+        );
+    }
+
+    /*
+     * BracketArgText = (!Newline !InlineElement ("\\\\" / "\\]" / "\\" / !"]" .))+
+     */
+    Rule BracketArgText() {
+        return OneOrMore(
+            TestNot(Newline()),
+            TestNot(InlineElement()),
+            FirstOf("\\\\", "\\]", "\\", Sequence(TestNot("]"), ANY))
+        );
+    }
+
+    /*
+     * BraceArg = "{" ([^\r\n\}\\]+ ("\\\\" / "\\}" / "\\")?)* "}"
+     */
+    Rule BraceArg() {
+        return Sequence(
+            "{",
+            ZeroOrMore(OneOrMore(NoneOf(new char[] {'\r', '\n', '}', '\\'})),
+                Optional(FirstOf("\\\\", "\\}", "\\"))),
+            "}"
+        );
+    }
+
+    /*
+     * BlockElementContents = BlockElementContent BlockElementContents? BlankLines
+     */
+    Rule BlockElementContents() {
+        return Sequence(
+            BlockElementContent(),
+            Optional(BlockElementContents()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * BlockElementContent = SingleLineComment / BlockElement / UList / OList / DList / BlockElementParagraph
+     */
+    Rule BlockElementContent() {
+        return FirstOf(
+            SingleLineComment(),
+            BlockElement(),
+            UList(),
+            OList(),
+            DList(),
+            BlockElementParagraph()
+        );
+    }
+
+    /*
+     * BlockElementParagraph = BlockElementParagraphSubs BlankLines
+     */
+    Rule BlockElementParagraph() {
+        return Sequence(
+            BlockElementParagraphSubs(),
+            BlankLines()
+        );
+    }
+
+    /*
+     * BlockElementParagraphSubs = BlockElementParagraphSub BlockElementParagraphSubs?
+     */
+    Rule BlockElementParagraphSubs() {
+        return Sequence(
+            BlockElementParagraphSub(),
+            Optional(BlockElementParagraphSubs())
+        );
+    }
+
+    /*
+     * BlockElementParagraphSub = InlineElement / BlockElementContentText
+     */
+    Rule BlockElementParagraphSub() {
+        return FirstOf(
+            InlineElement(),
+            BlockElementContentText()
+        );
+    }
+
+    /*
+     * BlockElementContentText = (&. !"//}" !SingleLineComment !BlockElement !UList !OList !DList
+     *                               (!InlineElement [^\r\n])+ Newline?)+
+     */
+    Rule BlockElementContentText() {
+        return OneOrMore(
+            Test(ANY),
+            TestNot("//}"),
+            TestNot(SingleLineComment()),
+            TestNot(BlockElement()),
+            TestNot(UList()),
+            TestNot(OList()),
+            TestNot(DList()),
+            OneOrMore(TestNot(InlineElement()), NoneOf(new char[] {'\r', '\n'})),
+            Optional(Newline())
+        );
+    }
+
+    /*
+     * InlineElementContents = !"}" InlineElementContent InlineElementContents?
+     */
+    Rule InlineElementContents() {
+        return Sequence(
+            TestNot("}"),
+            InlineElementContent(),
+            Optional(InlineElementContents())
+        );
+    }
+
+    /*
+     * InlineElementContent = InlineElement / InlineElementContentText
+     */
+    Rule InlineElementContent() {
+        return FirstOf(
+            InlineElement(),
+            InlineElementContentText()
+        );
+    }
+
+    /*
+     * InlineElementContentText = (!InlineElement [^\r\n}])+
+     */
+    Rule InlineElementContentText() {
+        return OneOrMore(
+            TestNot(InlineElement()),
+            NoneOf(new char[] {'\r', '\n'})
+        );
+    }
+
+    /*
+     * SingleLineContent = ContentInlines (Newline / EOI)
+     */
+    Rule SingleLineContent() {
+        return Sequence(
+            ContentInlines(),
+            FirstOf(Newline(), EOI)
+        );
+    }
+
+    /*
+     * ContentInlines = ContentInline ContentInlines?
+     */
+    Rule ContentInlines() {
+        return Sequence(
+            ContentInline(),
+            Optional(ContentInlines())
+        );
+    }
+
+    /*
+     * ContentInline = InlineElement / ContentInlineText
+     */
+    Rule ContentInline() {
+        return FirstOf(
+            InlineElement(),
+            ContentInlineText()
+        );
+    }
+
+    /*
+     * ContentInlineText = (!InlineElement [^\r\n])+
+     */
+    Rule ContentInlineText() {
+        return OneOrMore(
+            TestNot(InlineElement()),
+            NoneOf(new char[] {'\r', '\n'})
+        );
+    }
+
+    /*
+     * UList = (UListElement / SingleLineComment) UList? BlankLines
+     */
+    Rule UList() {
+        return Sequence(
+            FirstOf(UListElement(), SingleLineComment()),
+            Optional(UList()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * UListElement = " "+ "*"+ Space* SingleLineContent
+     */
+    Rule UListElement() {
+        return Sequence(
+            OneOrMore(" "),
+            OneOrMore("*"),
+            ZeroOrMore(Space()),
+            SingleLineContent()
+        );
+    }
+
+    /*
+     * OList = (OListElement / SingleLineComment) OList? BlankLines
+     */
+    Rule OList() {
+        return Sequence(
+            FirstOf(OListElement(), SingleLineComment()),
+            Optional(OList()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * OListElement = " "+ Digits "." Space* SingleLineContent
+     */
+    Rule OListElement() {
+        return Sequence(
+            OneOrMore(" "),
+            Digits(),
+            ".",
+            ZeroOrMore(Space()),
+            SingleLineContent()
+        );
+    }
+
+    /*
+     * DList = (DListElement / SingleLineComment) DList? BlankLines
+     */
+    Rule DList() {
+        return Sequence(
+            FirstOf(DListElement(), SingleLineComment()),
+            Optional(DList()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * DListElement = " "* ":" " " Space* SingleLineContent DListElementContents BlankLines
+     */
+    Rule DListElement() {
+        return Sequence(
+            ZeroOrMore(" "),
+            ":",
+            " ",
+            ZeroOrMore(Space()),
+            SingleLineContent(),
+            DListElementContents(),
+            BlankLines()
+        );
+    }
+
+    /*
+     * DListElementContents = DListElementContent DListElementContents? BlankLines
+     */
+    Rule DListElementContents() {
+        return Sequence(
+            DListElementContent(),
+            Optional(DListElementContents()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * DListElementContent = [ \t]+ SingleLineContent Newline?
+     */
+    Rule DListElementContent() {
+        return Sequence(
+            OneOrMore(AnyOf(new char[] {' ', '\t'})),
+            SingleLineContent(),
+            Optional(Newline())
+        );
+    }
+
+    /*
+     * SingleLineComments = SingleLineComment SingleLineComments?
+     */
+    Rule SingleLineComments() {
+        return Sequence(
+            SingleLineComment(),
+            Optional(SingleLineComments())
+        );
+    }
+
+    /*
+     * SingleLineComment = "#@" [^\r\n]* Newline? BlankLines
+     */
+    Rule SingleLineComment() {
+        return Sequence(
+            "#@",
+            ZeroOrMore(NoneOf(new char[] {'\r', '\n'})),
+            Optional(Newline()),
+            BlankLines()
+        );
+    }
+
+    /*
+     * Digits = Digit+
+     */
+    Rule Digits() {
+        return OneOrMore(
+            Digit()
+        );
+    }
+
+    /*
+     * Digit = [0-9]
+     */
+    Rule Digit() {
+        return CharRange(
+            '0',
+            '9'
+        );
+    }
+
+    /*
+     * LowerAlphabet = [a-z]
+     */
+    Rule LowerAlphabet() {
+        return CharRange(
+            'a',
+            'z'
+        );
+    }
+
+    /*
+     * Newline = "\r\n" / "\n"
+     */
+    Rule Newline() {
+        return FirstOf(
+            "\r\n",
+            "\n"
+        );
+    }
+
+    /*
+     * BlankLines = ([ \t]* Newline)*
+     */
+    Rule BlankLines() {
+        return ZeroOrMore(
+            ZeroOrMore(AnyOf(new char[] {' ', '\t'})),
+            Newline()
+        );
+    }
+
+    /*
+     * Spacer = [ \t\r\n]*
+     */
+    Rule Spacer() {
+        return ZeroOrMore(
+            AnyOf(new char[] {' ', '\t', '\r', '\n'})
+        );
+    }
+
+    /*
+     * Space = [ 　\t]
+     */
+    Rule Space() {
+        return AnyOf(
+            new char[] {' ', '　', '\t'}
         );
     }
 

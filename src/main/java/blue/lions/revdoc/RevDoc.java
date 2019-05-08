564eebb5a31e4d865a7c53ae724ae3b01d39d6a6
Jun-ya HASEBA <7pairs@gmail.com>
2019-05-08 18:02:18 +0900

    Update RevDoc class #30

--- src/main/java/blue/lions/revdoc/RevDoc.java
+++ src/main/java/blue/lions/revdoc/RevDoc.java
@@ -33,11 +33,11 @@ public class RevDoc {
         // コマンドライン引数をパースする
         Arguments arguments = Arguments.parse(args);
 
-        // 原稿ファイルを解析する
+        // 原稿を抽象構文木に変換する
         Reader reader = Reader.getInstance(arguments);
         Node ast = reader.run();
 
-        // 原稿ファイルの内容を出力する
+        // 抽象構文木の内容を出力する
         Writer writer = Writer.getInstance(arguments);
         writer.run(ast);
     }

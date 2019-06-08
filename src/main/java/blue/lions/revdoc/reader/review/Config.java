337a9ba11a749467ffdd375fc5e859e5ec63d3a9
HASEBA, Junya <7pairs@gmail.com>
2019-06-08 21:14:02 +0900

    Update with temporary implementation #30

--- src/main/java/blue/lions/revdoc/reader/review/Config.java
+++ src/main/java/blue/lions/revdoc/reader/review/Config.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Jun-ya HASEBA
+ * Copyright 2019 HASEBA, Junya
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
